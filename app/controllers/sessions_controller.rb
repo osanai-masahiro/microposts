1ce3481f71b14d9ecb94bf7cb3a06afb51b6e09c
MsahiroOsanai <osanai.masahiro.kakaku@gmail.com>
2016-04-22 06:16:10 +0000

    finished login function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+
   def create
-    @user = User.find_by(email: params[:session][:email].downcase) &&
+    @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       flash[:info] = "logged in as #{@user.name}"
       redirect_to @user
     else
-      flash[:danger] = "invalid email/password combination"
-      render "new"
+      flash[:danger] = 'invalid email/password combination'
+      render 'new'
     end
   end
   
@@ -18,5 +18,4 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_path
   end
-  
 end
