90a41605468ccc3c30fe3d67079153abc3878c0d
MsahiroOsanai <osanai.masahiro.kakaku@gmail.com>
2016-04-25 08:35:47 +0000

    follow and follower

--- db/schema.rb
+++ db/schema.rb
@@ -13,16 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160423092840) do
 
-  create_table "microposts", force: :cascade do |t|
-    t.integer  "user_id"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
-  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
-
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
     t.integer  "followed_id"
